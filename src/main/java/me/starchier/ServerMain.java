f43e198d0b8a1c9fe73980db6bcbffb926b59aa1
StarchierOrb <tiyanw8@hotmail.com>
2021-04-28 00:34:04 +0800

    Fix build.gradle

--- src/main/java/me/starchier/ServerMain.java
+++ src/main/java/me/starchier/ServerMain.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 public class ServerMain {
-    public static final String VERSION = "1.0.b38-SNAPSHOT";
+    public static final String VERSION = "1.0.b39-SNAPSHOT";
     public static final String NAME = "SmartApp-Server";
     private static final Logger getLogger = LogManager.getLogger("ServerMain");
     private static final String prompt = ">";
