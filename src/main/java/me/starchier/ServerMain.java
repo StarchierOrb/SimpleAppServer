a98969f8d7b5c3ca6cf7e031b3eef2ad33947ff2
StarchierOrb <tiyanw8@hotmail.com>
2021-04-27 20:14:47 +0800

    完成log4j与jline3的配置

--- src/main/java/me/starchier/ServerMain.java
+++ src/main/java/me/starchier/ServerMain.java
@@ -1,5 +1,7 @@
 package me.starchier;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jline.reader.EndOfFileException;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
@@ -10,13 +12,15 @@ import org.jline.terminal.TerminalBuilder;
 import java.io.IOException;
 
 public class ServerMain {
-    public static final String VERSION = "1.0.b2-SNAPSHOT";
+    public static final String VERSION = "1.0.b21-SNAPSHOT";
+    private static final Logger getLogger = LogManager.getLogger("Main");
     private static final String prompt = ">";
     public static void main(String[] args) {
         Terminal terminal;
         LineReader lineReader = null;
+        getLogger.info("服务端已启动。");
         try {
-            terminal = TerminalBuilder.builder().system(true).build();
+            terminal = TerminalBuilder.builder().system(true).dumb(true).build();
             lineReader = LineReaderBuilder.builder().terminal(terminal).build();
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,10 +30,23 @@ public class ServerMain {
             try {
                 assert lineReader != null;
                 line = lineReader.readLine(prompt);
+                switch (line) {
+                    case "version": {
+                        getLogger.info(VERSION);
+                        continue;
+                    }
+                    case "stop": {
+                        getLogger.info("接收到停止命令。");
+                        getLogger.info("正在停止服务器...");
+                        return;
+                    }
+                    default: {
+                        getLogger.info("未知的指令。");
+                    }
+                }
             } catch (UserInterruptException e) {
-
             } catch(EndOfFileException e) {
-
+                getLogger.info("正在关闭服务器...");
             }
         }
     }
