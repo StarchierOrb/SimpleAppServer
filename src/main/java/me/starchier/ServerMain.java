7586e854543cec4196f978f6f61d61d77c759efa
StarchierOrb <tiyanw8@hotmail.com>
2021-04-29 13:22:42 +0800

    Update changeable project name

--- src/main/java/me/starchier/ServerMain.java
+++ src/main/java/me/starchier/ServerMain.java
@@ -18,8 +18,8 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 public class ServerMain {
-    public static final String VERSION = "1.0.46-DEV-SNAPSHOT";
-    public static final String NAME = "SmartApp-Server";
+    public static final String VERSION = "1.0.48-DEV-SNAPSHOT";
+    public static final String NAME = "SmartApp-Server-TEST";
     private static final Logger getLogger = LogManager.getLogger("ServerMain");
     private static final String prompt = ">";
     public static void main(String[] args) {
